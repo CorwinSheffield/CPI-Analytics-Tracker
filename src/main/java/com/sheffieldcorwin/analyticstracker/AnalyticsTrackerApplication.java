cc19a56616fb2baa5b14a8ed4bc9e37d5774dc18
Corwin Sheffield <sheffieldcorwin@gmail.com>
2025-09-10 12:04:31 -0400

    Application class and appconfig changes

--- src/main/java/com/sheffieldcorwin/analyticstracker/AnalyticsTrackerApplication.java
+++ src/main/java/com/sheffieldcorwin/analyticstracker/AnalyticsTrackerApplication.java
@@ -1,42 +1,19 @@
 package com.sheffieldcorwin.analyticstracker;
 
-import java.util.List;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.sheffieldcorwin.analyticstracker.entity.FinanceTransaction;
-import com.sheffieldcorwin.analyticstracker.service.CSVImportService;
-import com.sheffieldcorwin.analyticstracker.service.FinanceTransactionService;
-import com.sheffieldcorwin.analyticstracker.service.TransactionGeneratorService;
-import com.sheffieldcorwin.analyticstracker.service.ValidationResult;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AnalyticsTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnalyticsTrackerApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner runAtStartup(TransactionGeneratorService generatorService, CSVImportService csvImportService, FinanceTransactionService financeService) {
-//
-//		return args -> {
-//			System.out.println("=== Generating Data...===");
-//			generatorService.generateCsv("src/main/resources/data/transactions.csv", 10000, 0.07);
-//			System.out.println("=== importing CSV at startup ===");
-//			List<FinanceTransaction> importedTransactions = csvImportService
-//					.importTransactions("src/main/resources/data/transactions.csv");
-//			System.out.println("=== CSV import Complete ===");
-//			System.out.println("=== Processing Transactions ===");
-//			ValidationResult result = financeService.processTransactions(importedTransactions);
-//			System.out.println("=== Processing Complete ===");
-//
-//			System.out.println("Number of valid transactions: " + result.getValid().size()
-//					+ "\nNumber of invalid transactions: " + result.getInvalid().size());
-//		};
-//	}
+	
+
+	
 
 }
--- src/main/java/com/sheffieldcorwin/analyticstracker/config/AppConfig.java
+++ src/main/java/com/sheffieldcorwin/analyticstracker/config/AppConfig.java
@@ -1,18 +1,17 @@
 package com.sheffieldcorwin.analyticstracker.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.time.Clock;
+import java.time.ZoneId;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application-${spring.profiles.active}.properties")
 public class AppConfig {
 	
-	@Value("${obp.api.key}")
-	private String apiKey;
-	@Value("${obp.api.secret}")
-	private String apiSecret;
-	@Value("${obp.api.url}")
-	private String apiUrl;
-
+	@Bean
+	Clock clock() {
+		return Clock.system(ZoneId.of("America/New_York"));
+	}
+  
 }
