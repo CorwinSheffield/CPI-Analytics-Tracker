b37bb6828a100f13c11e692026ffc294c39d747e
Corwin Sheffield <sheffieldcorwin@gmail.com>
2025-09-10 11:54:48 -0400

    Removed obsolete files

--- src/main/java/com/sheffieldcorwin/analyticstracker/repository/FinanceTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sheffieldcorwin.analyticstracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sheffieldcorwin.analyticstracker.entity.FinanceTransaction;
-
-public interface FinanceTransactionRepository extends JpaRepository<FinanceTransaction, Long>{
-	
-}
